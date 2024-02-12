74a0b336c3583e1479ecfaded088accf4d59b3ff
ydbweb <ydbweb.com@gmail.com>
2024-02-12 14:32:44 +0100

    initial commit

---
+++ src/test/java/com/example/restpost/RestpostApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.restpost;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestpostApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
